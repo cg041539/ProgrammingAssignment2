25e5b4f7073762883e73743a5876a9e443691a3e
cg041539 <39295297+cg041539@users.noreply.github.com>
2020-07-14 22:50:47 +0200

    Commit function with comments
    Bla bla bla

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This fucntions takes an invertible matrix and has the capacity to store
+## the inverse of the given matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        s <- NULL       ## variable to store the inverse of the given matrix
+        set <- function (y) {
+                x <<-y
+                s <<-NULL ## We flush any store inverse previously stored
+        }
+        get <- function () x
+        setsolve <- function(solve) s <<-solve
+        getsolve <- function() s
+        list(set=set, get=get, setsolve=setsolve, getsolve=getsolve) 
 }
 
 
-## Write a short comment describing this function
+## This functions calculates the inverse (solve) of a given invertible matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        s <- x$getsolve()
+        if(!is.null(s)) { ## If the inverse is already cached, don't solve it again
+                message("getting cached data")
+                return(s)
+        }
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setsolve(s)
+        s
 }
